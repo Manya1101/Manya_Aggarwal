3d840bc7b4ba3cf4b50357a669466d32d147c689
Manya Aggarwal <146344077+Manya1101@users.noreply.github.com>
2025-11-30 01:33:30 +0530

    Create Problem2.cpp

---
+++ Problem2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cout << (2*i + 1);
+        if (i != n-1) cout << ", ";
+    }
+    cout << endl;
+
+    return 0;
+}
